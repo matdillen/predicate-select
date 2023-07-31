5081cf934298b5b14646c01f9ec35fcefc3b5550
Mathias Dillen <37537540+matdillen@users.noreply.github.com>
2023-07-31 15:49:39 +0200

    almost fixed add_to_wd.py
    the object for addSources still is not entirely correct, clearly

--- add_to_wd.py
+++ add_to_wd.py
@@ -1,43 +1,55 @@
 import pywikibot
 
 def remove_reference(item_id, property_id, target_id, reference_string):
-    site = pywikibot.Site("wikidata", "wikidata")
-    repo = site.data_repository()
+site = pywikibot.Site("wikidata", "wikidata")
+repo = site.data_repository()
 
-    # Get the item that has the statement with the specified property and target
-    item = pywikibot.ItemPage(repo, item_id)
-    statement = pywikibot.Claim(repo, property_id)
-    statement.setTarget(pywikibot.ItemPage(repo, target_id))
+# Get the item that has the statement with the specified property and target
+item = pywikibot.ItemPage(repo, item_id)
+statement = pywikibot.Claim(repo, property_id)
+statement.setTarget(pywikibot.ItemPage(repo, target_id))
 
-    # Load the item to access the statements
-    item.get()
+# Load the item to access the statements
+item.get()
 
-    # Check if the statement exists and has any references
-    if statement.getID() in item.get().get("claims"):
-        for claim in item.get().get("claims")[statement.getID()]:
-            if claim.sources:
-                new_references = claim.sources
-                replace = False
-                i = 0
-                for reference in claim.sources:
-                    for value in reference:
-                        for s in snak:
-                            if s["snaktype"] == "value" and s["datavalue"]["value"] == reference_string:
-                                replace = True
-                                del new_references[i]
-                    i = i + 1
-                if replace:
-                    claim.changeReferences(new_references) #this method doesn't exist
-                    return
+if statement.getID() in item.get().get("claims"):
+    for claim in item.get().get("claims")[statement.getID()]:
+        if "references" in claim.toJSON():
+            # Filter and remove the specified reference
+            new_references = []
+            do_edit = False
+            for reference in claim.toJSON()["references"]:
+                keep_reference = True
+                test = reference
+                for snaks in reference["snaks"]:
+                    for snak in reference["snaks"][snaks]:
+                        if snak.get("snaktype") == "value" and snak.get("datavalue").get("value") == reference_string:
+                            keep_reference = False
+                            do_edit = True
+                            break
+                    if not keep_reference:
+                        break
+                if keep_reference:
+                    new_references.append(reference)
+            if do_edit:
+                claim.removeSources(claim.sources)
+                print("remove:\n")
+                print(claim.sources)
+                for reference in new_references:
+                    print("\nadd:\n")
+                    print([reference])
+                    claim.addSources([reference], summary="Removing specified reference")
 
-    print("Statement or reference not found.")
+                print("Reference removed successfully.")
+                return
+
+print("Statement or reference not found.")
 
 
-if __name__ == "__main__":
-    # Example usage
-    item_id = "Q1333160"
-    property_id = "P11146"
-    target_id = "Q15820827"
-    reference_string = "HTTP://LAGU"
+# Example usage
+item_id = "Q1333160"
+property_id = "P11146"
+target_id = "Q15820827"
+reference_string = "HTTP://LAGU"
 
-    remove_reference(item_id, property_id, target_id, reference_string)
+remove_reference(item_id, property_id, target_id, reference_string)
